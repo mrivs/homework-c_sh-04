af8352ddc1cd70e66b47cd60ce23793e574a8797
mrivs <valentinilyins@gmail.com>
2022-10-13 04:55:23 +0300

    task 03 added

--- task 03/Program.cs
+++ task 03/Program.cs
@@ -7,3 +7,46 @@
 
 6, 1, 33 -> [6, 1, 33]
 /*/
+
+int N = PrintAndGetValue("Введите длинну массива");
+int min = PrintAndGetValue("Введите min");
+int max = PrintAndGetValue("Введите max");
+
+if (N > 0 && max >= min)
+{
+    int[] arr = CreateArray(N, min, max);
+    Console.WriteLine(PrintArray(arr));
+}
+else {
+    Console.WriteLine("Неверные входные значения");
+}
+
+int[] CreateArray(int N, int min, int max)  // создаем массив длинной N и значениями в диапазоне [min, max]
+{
+    Random rnd = new Random();
+    int[] array = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+string PrintArray(int[] array) // пишем массив в строку
+{
+    string str = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) str = str + array[i] + ", ";
+        else str = str + array[i] + "]";
+    }
+    return str;
+}
+
+
+int PrintAndGetValue(string message)
+{
+    Console.WriteLine(message);
+    int v = int.Parse(Console.ReadLine());
+    return v;
+}
